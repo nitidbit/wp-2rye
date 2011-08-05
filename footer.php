01387a2f0adebbbb316e931806c9366ce103e43a
Mr Rogers <jon@bunnymatic.com>
2011-08-05 11:42:57 -0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div id="footer">
+   <h3>Copyright</h3>
+   <div class="copyright">
+   <p>Copyright &copy; 2011 by Rogers Yun Enterprises (aka 2RYE). All rights reserved.</p>
+   </div>
+<!-- This theme is free and you can alter it as you see fit, but please keep the link to Upstart Blogger. Thanks! -->
+   <div class="credits"><p>Template originally designed by <a href="http://www.upstartblogger.com/">Upstart Blogger</a>, customized by <a href="http://www.2rye.com">2RYE</a>.
+   </div>
+                      
+                      
+                      <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+</div><!-- end footer -->
+		<?php wp_footer(); ?>
+</body>
+</html>
