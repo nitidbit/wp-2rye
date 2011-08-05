42ae893e015827fa6724b5806c19f831fccafc0b
Mr Rogers <jon@bunnymatic.com>
2011-08-05 15:48:48 -0700

    * added credit to david h

--- footer.php
+++ footer.php
@@ -1,11 +1,15 @@
 <div id="footer">
-   <h3>Copyright</h3>
-   <div class="copyright">
-   <p>Copyright &copy; 2011 by Rogers Yun Enterprises (aka 2RYE). All rights reserved.</p>
-   </div>
+   <div class='row'>
+     <h3>Copyright</h3>
+     <div class="copyright">
+     <p>Copyright &copy; 2011 by Rogers Yun Enterprises (aka 2RYE). All rights reserved.</p>
+     </div>
 <!-- This theme is free and you can alter it as you see fit, but please keep the link to Upstart Blogger. Thanks! -->
-   <div class="credits"><p>Template originally designed by <a href="http://www.upstartblogger.com/">Upstart Blogger</a>, customized by <a href="http://www.2rye.com">2RYE</a>.
+     <div class="credits"><p>Template originally designed by <a href="http://www.upstartblogger.com/">Upstart Blogger</a>, customized by <a href="http://www.2rye.com">2RYE</a>.
+     </div>
    </div>
+   <div class="logo_credits row">
+                         Logo design: <a href="http://blog.carbonfive.com/author/dhendee/">David Hendee, Director of Design at Carbon Five</a>.
                       
                       
                       <!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
