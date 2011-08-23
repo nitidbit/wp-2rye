c578e65fbe584f1f898f1fe783306501c4e21a49
Mr Rogers <jon@bunnymatic.com>
2011-08-23 12:19:55 -0700

    * remove categories in favor of tags

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@
         <li>
  
           <div class="results_content">
-           <h3><div><?php the_category(', ') ?><div></h3><h1 style="display:inline;"><a href="<?php the_permalink(); ?>" style="color:#444444;"><?php the_title(); ?></a></h1><div style="margin-top:-2px;"><small><?php the_time('m.d.y'); ?> <b>|</b> <a href="<?php the_permalink(); ?>">Permalink</a> <b>|</b> <?php comments_popup_link('Comment?', '1 Comment', '% Comments' ) ?> </small></div>
+           <h3><div><?php the_tags() ?><div></h3><h1 style="display:inline;"><a href="<?php the_permalink(); ?>" style="color:#444444;"><?php the_title(); ?></a></h1><div style="margin-top:-2px;"><small><?php the_time('m.d.y'); ?> <b>|</b> <a href="<?php the_permalink(); ?>">Permalink</a> <b>|</b> <?php comments_popup_link('Comment?', '1 Comment', '% Comments' ) ?> </small></div>
 
             <div style="margin-top: -1em;">
               <?php the_excerpt(); ?>
