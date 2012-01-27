43ecbf8d695a7476e4e5b5f0465807a0e41c4af1
Mr Rogers <jon@bunnymatic.com>
2012-01-27 09:45:15 -0800

    * cleanup search results a bit

--- search.php
+++ search.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 <div id="container">
-<div id="entries">
+<div id="entries" class='search_results'>
 <?php previous_post_link('&laquo; %link','%title') ?><br/>
 <?php next_post_link('&raquo; %link','%title') ?>	
 
 	<h3>Look What I Found</h3>
-<h1 style="display:inline; font-size:3.3em;">Search Results</h1><br/> <br/>
-      <br/>
 	<?php if (have_posts()) : ?>
 	 <ul>
         <?php while (have_posts()) : the_post(); ?>
@@ -14,7 +12,7 @@
         <li>
  
           <div class="results_content">
-           <h3><div><?php the_category(', ') ?></div></h3><h1 style="display:inline;"><a href="<?php the_permalink(); ?>" style="color:#444444;"><?php the_title(); ?></a></h1><div style="margin-top:-2px;"><small><?php the_time('m.d.y'); ?> <b>|</b> <a href="<?php the_permalink(); ?>">Permalink</a> <b>|</b> <?php comments_popup_link('Comment?', '1 Comment', '% Comments' ) ?> </small></div>
+           <h1 style="display:inline;"><a href="<?php the_permalink(); ?>" style="color:#444444;"><?php the_title(); ?></a></h1><div style="margin-top:-2px;"><small><?php the_time('m.d.y'); ?> <b>|</b> <a href="<?php the_permalink(); ?>">Permalink</a> <b>|</b> <?php comments_popup_link('Comment?', '1 Comment', '% Comments' ) ?> </small></div>
 
             <div style="margin-top: -1em;">
               <?php the_excerpt(); ?>
